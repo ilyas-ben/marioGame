f5ba9db0f6dfafcf7035b8a109762f4d2aaaf2a2
ilyas-ben <ilyasben313@gmail.com>
2024-11-19 10:31:33 +0200

    adding pacman game

---
+++ src/main/java/com/ilouse/mario/games/Pacman.java
@@ -0,0 +1,19 @@
+package com.ilouse.mario.games;
+
+public class Pacman implements GamingConsole {
+    public void up() {
+        System.out.println("up");
+    }
+
+    public void down() {
+        System.out.println("down  ");
+    }
+
+    public void left() {
+        System.out.println("left  ");
+    }
+
+    public void right() {
+        System.out.println("right");
+    }
+}
