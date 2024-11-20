b9d978a63203f4038b5ecc3b24baa0545330383f
ilyas-ben <ilyasben313@gmail.com>
2024-11-20 11:17:23 +0200

    Reorganizing classes and clean the code

--- src/main/java/com/ilouse/mario/AppGamingInSpring.java
+++ src/main/java/com/ilouse/mario/AppGamingInSpring.java
@@ -3,7 +3,10 @@ package com.ilouse.mario;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.ilouse.mario.games.GameRunner;
 import com.ilouse.mario.games.GamingConsole;
@@ -11,19 +14,44 @@ import com.ilouse.mario.games.MarioGame;
 import com.ilouse.mario.games.Pacman;
 import com.ilouse.mario.games.SuperContraGame;
 
-public class AppGamingInSpring {
-  public static void main(String[] args) {
-    var context = new AnnotationConfigApplicationContext(AppGamingConfiguration.class);
 
-    var marioGame = context.getBean(MarioGame.class);
-    var superContraGame = context.getBean(SuperContraGame.class);
-    var pacmac = context.getBean(Pacman.class);
+@Configuration
+public class AppGamingInSpring {
+
+  @Bean
+  public String myName() {
+    return "ilyasou";
+  }
+
+  @Bean
+  public GamingConsole mario() {
+    return new MarioGame();
+  }
+
+  @Bean
+  public GamingConsole superContraGame() {
+    return new SuperContraGame();
+  }
+
+  @Bean
+  public GamingConsole pacman() {
+    return new Pacman();
+  }
+
+  @Bean
+  public GameRunner gameRunner(@Qualifier("mario") GamingConsole gamingConsole) {
+    return new GameRunner(gamingConsole);
+  }
+  
+
+  public static void main(String[] args) {
+    var context = new AnnotationConfigApplicationContext(AppGamingInSpring.class);
 
     var gameRunner = context.getBean(GameRunner.class);
-    gameRunner.run();    
+    gameRunner.run();
 
     System.out.println("Below all the beans created : \n \n ");
-    
+
     Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println); // displaying all beans
 
     context.close();
