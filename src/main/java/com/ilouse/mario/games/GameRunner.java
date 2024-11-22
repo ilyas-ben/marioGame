469b8c7f7872da780bd1b7d931dd15ff48e6f1a9
ilyas-ben <ilyasben313@gmail.com>
2024-11-22 10:32:51 +0200

    discovering the setter based injection

--- src/main/java/com/ilouse/mario/games/GameRunner.java
+++ src/main/java/com/ilouse/mario/games/GameRunner.java
@@ -1,18 +1,24 @@
 package com.ilouse.mario.games;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GameRunner {
 
-    @Autowired
+    //@Autowired
     private GamingConsole game;
 
     public GameRunner(GamingConsole game) {
         this.game = game;
     }
+
+    @Autowired
+    public void setGame(GamingConsole game) {
+        System.out.println("game"+game+"have been injected thr setter");
+        this.game = game;
+    }
+    
 
     public void run() {
         // TODO Auto-generated method stub
@@ -24,3 +30,9 @@ public class GameRunner {
     }
 
 }
+
+
+
+
+
+
